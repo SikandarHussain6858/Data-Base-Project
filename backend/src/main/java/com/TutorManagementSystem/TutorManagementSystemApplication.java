d25bed75e2e0c488afac1ecb7d2dea5ad11071b7
ZiA-rR <zrehman.bsai24seecs@seecs.edu.pk>
2025-04-25 19:25:57 +0500

    changes by zia

---
+++ backend/src/main/java/com/TutorManagementSystem/TutorManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.TutorManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TutorManagementSystemApplication.class, args);
+    }
+}
+
